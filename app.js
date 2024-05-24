88073be33e6d962f1b561b91808cc24d3244e4fd
Akhil Mahesh <akhilmahesh@proton.me>
2024-05-24 13:49:27 +0530

    Minor Changes

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(
   session({
-    secret: "your-secret-key",
+    secret: "aydyrtcrkvtbSFT46sFSterutee564POn",
     resave: false,
     saveUninitialized: false,
   })
