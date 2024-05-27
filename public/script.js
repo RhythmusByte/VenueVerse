e3d10db33454931830dc33da25f6bdf2084b1f78
Akhil Mahesh <akhilmahesh@proton.me>
2024-05-27 11:16:32 +0530

    JavaScript for Register

---
+++ public/script.js
@@ -0,0 +1,27 @@
+document.getElementById("signup-form").addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const form = event.target;
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData.entries());
+
+  try {
+    const response = await fetch("/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    const result = await response.json();
+    const signupErrorElement = document.getElementById("signup-error");
+    if (response.status !== 201) {
+      signupErrorElement.textContent = result.error;
+    } else {
+      signupErrorElement.textContent = "";
+      window.location.href = "index.html";
+    }
+  } catch (error) {
+    console.error("Error during registration:", error);
+  }
+});
